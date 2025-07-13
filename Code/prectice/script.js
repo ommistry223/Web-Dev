aca90c7ab6d15241a6327ae447051917e6e0a2af
ommistry223 <ommistry5559@gmail.com>
2025-07-13 23:04:59 +0530

    Fianl commit

---
+++ Code/prectice/script.js
@@ -0,0 +1,9 @@
+function myfuntion()
+        {
+        var str = "Hello world"
+        str = "122344"
+        console.log(str)
+        alert(str)
+        document.write("Om")
+        document.write("<b><br>",str,"b")
+        }
