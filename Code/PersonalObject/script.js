f62c47b2706dcdf1131e3db83bf79a954fecbbed
ommistry223 <ommistry5559@gmail.com>
2025-08-13 23:51:19 +0530

    Final commit

---
+++ Code/PersonalObject/script.js
@@ -0,0 +1,16 @@
+const person = {
+  id: "24CE065",
+  name: "OM MISTRY",
+  age: 19,
+  gender: "Male",
+  email: "ommistry5551@gmail.com",
+  address: "Nadiad, Gujarat, India"
+};
+
+// Displaying the data in HTML
+document.getElementById("name").textContent = person.name;
+document.getElementById("id").textContent = person.id;
+document.getElementById("age").textContent = person.age;
+document.getElementById("gender").textContent = person.gender;
+document.getElementById("email").textContent = person.email;
+document.getElementById("address").textContent = person.address;
